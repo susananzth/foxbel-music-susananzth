cadaee1dd38e83f7221e3e41a67bbbf63be6cb8a
Susana Piñero <susananzth@gmail.com>
2021-03-23 19:14:30 -0500

    Separando Card de las canciones y comenzando conexión con API

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -3,89 +3,17 @@
         <Sidebar />
         <div id="layoutContent_main">
             <main class="px-5">
-              <NavBar />
-              <div class="row px-5">
-                  <div class="col-12 mb-3">
-                    <div class="row">
-                      <div class="col-3"><img src="https://source.unsplash.com/TIutDBFEtcY/400x400" class="card-img-top" alt="..."></div>
-                      <div class="col-9"></div>
+                <NavBar />
+                <div class="row px-5">
+                    <div class="col-12 mb-3">
+                        <div class="row">
+                        <div class="col-3"><img src="https://source.unsplash.com/TIutDBFEtcY/400x400" class="card-img-top" alt="..."></div>
+                        <div class="col-9"></div>
+                        </div>
                     </div>
-                  </div>
-                  <h3 class="text-dark">Resultados</h3>
-                  <div class="col-2 py-2">
-                      <div class="card">
-                          <i class="fas fa-ellipsis-v btn-option"></i>
-                          <i class="fas fa-play btn-play"></i>
-                          <img src="https://source.unsplash.com/TIutDBFEtcY/400x400" class="card-img-top" alt="...">
-                          <div class="card-body ">
-                              <p class="card-title">Título</p>
-                              <p class="card-text">Artista</p>
-                          </div>
-                      </div>
-                  </div>
-
-                  <div class="col-2 py-2">
-                      <div class="card">
-                          <i class="fas fa-ellipsis-v btn-option"></i>
-                          <img src="https://source.unsplash.com/TIutDBFEtcY/400x400" class="card-img-top" alt="...">
-                          <div class="card-body ">
-                              <p class="card-title">Título</p>
-                              <p class="card-text">Artista</p>
-                          </div>
-                      </div>
-                  </div>
-
-                  <div class="col-2 py-2">
-                      <div class="card">
-                          <i class="fas fa-ellipsis-v btn-option"></i>
-                          <img src="https://source.unsplash.com/TIutDBFEtcY/400x400" class="card-img-top" alt="...">
-                          <div class="card-body ">
-                              <p class="card-title">Título</p>
-                              <p class="card-text">Artista</p>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="col-2 py-2">
-                      <div class="card">
-                          <i class="fas fa-ellipsis-v btn-option"></i>
-                          <img src="https://source.unsplash.com/TIutDBFEtcY/400x400" class="card-img-top" alt="...">
-                          <div class="card-body ">
-                              <p class="card-title">Título</p>
-                              <p class="card-text">Artista</p>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="col-2 py-2">
-                      <div class="card">
-                          <i class="fas fa-ellipsis-v btn-option"></i>
-                          <img src="https://source.unsplash.com/TIutDBFEtcY/400x400" class="card-img-top" alt="...">
-                          <div class="card-body ">
-                              <p class="card-title">Título</p>
-                              <p class="card-text">Artista</p>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="col-2 py-2">
-                      <div class="card">
-                          <i class="fas fa-ellipsis-v btn-option"></i>
-                          <img src="https://source.unsplash.com/TIutDBFEtcY/400x400" class="card-img-top" alt="...">
-                          <div class="card-body ">
-                              <p class="card-title">Título</p>
-                              <p class="card-text">Artista</p>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="col-2 py-2">
-                      <div class="card">
-                          <i class="fas fa-ellipsis-v btn-option"></i>
-                          <img src="https://source.unsplash.com/TIutDBFEtcY/400x400" class="card-img-top" alt="...">
-                          <div class="card-body ">
-                              <p class="card-title">Título</p>
-                              <p class="card-text">Artista</p>
-                          </div>
-                      </div>
-                  </div>
-              </div>
+                    <h3 class="text-dark">Resultados</h3>
+                    <Card :cover="songs.album.cover" :album="songs.album.title" :title="songs.title" :artist="songs.artist.name" />
+                </div>
             </main>
         </div>
     </div>
@@ -93,16 +21,35 @@
 
 
 <script>
-import Sidebar from '../components/Sidebar.vue';
-import NavBar from '../components/NavBar.vue';
+import Sidebar from './Sidebar.vue';
+import NavBar from './NavBar.vue';
+import Card from "./Card.vue"
 
 export default {
-  name: 'Content',
-  components: {
+    data(){
+        return {
+            songs: null,
+        };
+    },
+    name: 'Content',
+    components: {
         Sidebar,
         NavBar,
-  }
-}
+        Card,
+    },
+    mounted() {
+        this.getSongs();
+    },
+    methods: {
+        async getSongs() {
+            const res = await fetch("https://api.deezer.com/search/track?q=eminem");
+            const data = await res.json();
+
+            this.songs = data;
+            console.log(this.songs);
+        },
+    },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -141,40 +88,5 @@ export default {
     font-size: 14px;
     line-height: 17px;
     text-align: center;
-}
-.card {
-    border: none;
-    border-radius: 0;
-}
-.card-img-top {
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-}
-.card-body {
-    padding: .2rem .1rem;
-}
-.card-title{
-    font-size: 14px;
-    font-weight: bold;
-    margin-bottom: 0;
-}
-.card-text{
-    font-size: 12px;
-}
-.btn-play{
-    color: #fff;
-    font-size: 36px;
-    position: absolute;
-    right: 50%;
-    text-shadow: 2px 0px 5px rgba(150, 150, 150, 1);
-    top: 50%;
-}
-.btn-option{
-    color: #fff;
-    float: right;
-    position: absolute;
-    right: 2%;
-    text-shadow: 2px 0px 5px rgba(150, 150, 150, 1);
-    top: 2%;
 }
 </style>
